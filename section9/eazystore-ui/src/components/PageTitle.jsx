71fab4da6607593432767d1d67974bb9fc2b4428
Shivp55 <shivparekh803@gmail.com>
2026-01-03 23:14:00 +0000

    React Web Dark Mode added and MySql SetUp

---
+++ section9/eazystore-ui/src/components/PageTitle.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+// Using Props for passing and getting data
+// export default function PageTitle(props){
+//     return <h1 className="page-title">{props.title}</h1>
+// }
+
+//Using destructuring
+export default function PageTitle({title}){  //using destructuring instead of writing props , use directly the key
+    return <h1 className="text-3xl font-primary font-extrabold text-center text-primary mt-4 py-2">{title}</h1>
+}
